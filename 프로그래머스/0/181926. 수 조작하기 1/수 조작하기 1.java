4314d10bee277f2e30325aac1a812f55f8bedd65
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-03-25 14:28:44 +0900

    [level 0] Title: 수 조작하기 1, Time: 8.05 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n, String control) {
+        char[] controlArr = control.toCharArray();
+        
+        for(char c : controlArr){
+            switch(c){
+            case 'w' :
+                n++;
+                break;
+            case 's':
+                n--;
+                break;
+            case 'd' :
+                n += 10;
+                break;
+            case 'a' :
+                n -= 10;
+                break;
+            }
+        }
+        
+        return n;
+    }
+}
