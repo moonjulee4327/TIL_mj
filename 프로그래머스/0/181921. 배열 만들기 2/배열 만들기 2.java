ac4ea9b06151d1566a290c193f4d201b1583b950
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-03-27 17:01:26 +0900

    [level 0] Title: 배열 만들기 2, Time: 58.97 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,43 +1,19 @@
-import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int l, int r) {
-        List<String> numList = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
-        int[] answer = null;
+        int[] answer = IntStream.range(l, r + 1).filter(n -> isOnlyZeroOrFiveNum(n)).toArray();
         
-        for(int i = l; i <= r; i++) {
-            numList.add(i + "");
-        }
-        
-        for(int i = 0; i < numList.size(); i++) {
-            String[] numArr = numList.get(i).split("");
-            boolean flag = false;
-            for(int j = 0; j < numArr.length; j++) { 
-                if(numArr[j].equals("0") || numArr[j].equals("5")) {
-                    flag = true;
-                }else {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) {
-                result.add(Integer.parseInt(numList.get(i)));
-            }else {
-                continue;
+        return answer.length == 0 ? new int[]{-1} : answer;
+    }
+    
+    private boolean isOnlyZeroOrFiveNum(int num) {
+        String str = String.valueOf(num);
+        for(char c : str.toCharArray()) {
+            if(c != '0' && c != '5') {
+                return false;
             }
         }
-        
-        if(result.size() == 0) {
-            result.add(-1);
-        }
-        
-        answer = new int[result.size()];
-        
-        for(int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);    
-        }
-        
-        return answer;
+        return true;
     }
 }
