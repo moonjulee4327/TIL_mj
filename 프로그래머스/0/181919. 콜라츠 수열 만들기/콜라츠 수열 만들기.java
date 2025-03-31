dd3746cd713ae2759ea9313da12a1f2e148c035c
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-03-31 23:22:01 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 6.62 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -1,21 +1,10 @@
-import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> answer = new ArrayList<>();
-        collatz(n, answer);
-        return answer.stream().mapToInt(i -> i).toArray();
-    }
-    
-    private void collatz(int n, List<Integer> answer) {
-        answer.add(n);
-        
-        if(n == 1) return;
-        
-        if(n % 2 == 0) {
-            collatz(n / 2, answer);
-        }else {
-            collatz(3 * n + 1, answer);
-        }
+        return IntStream.concat(
+            IntStream.iterate(n, i -> i > 1, i -> i % 2 == 0 ? i / 2 : 3 * i + 1),
+            IntStream.of(1)
+        ).toArray();
     }
 }
