d289886ad72e7f9073c3ee7660a4e77bcf79dc54
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-04-01 22:51:57 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,21 +1,5 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = true;
-        boolean x5 = true;
-        boolean x6 = true;
-        
-        if(x1 == false && x2 == false) {
-            x5 = false;
-        }
-        
-        if(x3 == false && x4 == false) {
-            x6 = false;
-        }
-        
-        if(x5 == false || x6 == false) {
-            answer = false;
-        }
-        
-        return answer;
+        return (x1||x2)&&(x3||x4);
     }
 }
