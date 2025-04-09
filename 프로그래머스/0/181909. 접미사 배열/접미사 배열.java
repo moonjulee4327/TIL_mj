cfdf3a71031f69ba29bd3dd5acbea773acdb73d7
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-04-09 16:17:19 +0900

    [level 0] Title: 접미사 배열, Time: 0.51 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,19 +1,14 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public String[] solution(String my_string) {
-        List<String> list = new ArrayList<>();
+        String[] answer = new String[my_string.length()];
         
         for(int i = 0; i < my_string.length(); i++) {
-            list.add(my_string.substring(i, my_string.length()));
+            answer[i] = my_string.substring(i, my_string.length());
         }
         
-        Collections.sort(list);
-        
-        String[] answer = new String[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        Arrays.sort(answer);
         
         return answer;
     }
