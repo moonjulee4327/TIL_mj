7cc134140d8b1d19add4b52ef59bac2550d7a6a6
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-04-11 13:25:02 +0900

    [level 0] Title: 문자열 뒤집기, Time: 15.98 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -2,8 +2,6 @@ class Solution {
     public String solution(String my_string, int s, int e) {
         StringBuilder answer = new StringBuilder(my_string.substring(s, e + 1));
         answer.reverse();
-        answer.insert(0, my_string.substring(0, s));
-        answer.append(my_string.substring(e + 1));
-        return answer.toString();
+        return my_string.substring(0, s) + answer + my_string.substring(e + 1);
     }
 }
