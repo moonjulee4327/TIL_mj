7469946c3c9a537d9450d5c0ede37940f69b0713
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-04-13 23:29:18 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.11 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -2,7 +2,8 @@ class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
         for(int i = 0; i < my_string.length(); i++) {
-            answer[my_string.charAt(i) >= 97 ? my_string.charAt(i) - 97 + 26: my_string.charAt(i) - 65] += 1;
+            char c = my_string.charAt(i);
+            answer[c >= 'a' ? c - 'a' + 26: c - 'A'] += 1;
         }
         return answer;
     }
