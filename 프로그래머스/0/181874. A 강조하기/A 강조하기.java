9228524ce1919f5e933e846762443975acb68b8f
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-04-23 21:09:38 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,13 +1,7 @@
 class Solution {
     public String solution(String myString) {
-        char[] answer = myString.toCharArray();
-        for(int i = 0; i < answer.length; i++) {
-            if(answer[i] == 'a' || answer[i] == 'A') {
-                answer[i] = Character.toUpperCase(answer[i]);
-            }else {
-                answer[i] = Character.toLowerCase(answer[i]);
-            }
-        }
-        return new String(answer);
+        myString = myString.toLowerCase();
+        myString = myString.replace("a", "A");
+        return myString;
     }
 }
