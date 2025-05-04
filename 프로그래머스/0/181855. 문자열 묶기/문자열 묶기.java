41ecf46c65affcfea2bb398ea8ce70255c67cbd3
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-05-04 18:01:25 +0900

    [level 0] Title: 문자열 묶기, Time: 8.87 ms, Memory: 131 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] strArr) {
+        int top = 0;
+        for(int i = 0; i < strArr.length; i++) {
+            if(strArr[i].length() > top) {
+                top = strArr[i].length();
+            }
+        }
+
+        int[] result = new int[top];
+        for(int i = 0; i < strArr.length; i++) {
+            result[strArr[i].length() - 1]++;
+        }
+        
+        top = 0;
+        for(int i = 0; i < result.length; i++) {
+            if(result[i] > top) {
+                top = result[i];
+            }
+        }
+
+        return top;
+    }
+}
