db185013a2e1509d017b4b7dcc0ecab5a8168731
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-05-10 16:20:37 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.05 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[][] arr) {
+        boolean flag = true;
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] != arr[j][i]) {
+                    flag = false;
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+}
