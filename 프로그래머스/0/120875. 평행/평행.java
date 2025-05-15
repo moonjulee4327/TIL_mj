8ca0686b9167567b6347625146d04bd2b7ba7eab
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-05-16 03:27:57 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,24 +1,17 @@
 class Solution {
+    int[][] dots;
+
     public int solution(int[][] dots) {
-        double line1, line2;
-
-        line1 = getLean(dots[0], dots[1]);
-        line2 = getLean(dots[2], dots[3]);
-        if (line1 == line2) return 1;
-
-        line1 = getLean(dots[0], dots[2]);
-        line2 = getLean(dots[1], dots[3]);
-        if (line1 == line2) return 1;
-
-        line1 = getLean(dots[0], dots[3]);
-        line2 = getLean(dots[1], dots[2]);
-        if (line1 == line2) return 1;
-
+        this.dots = dots;
+        if (parallel(0, 1, 2, 3)) return 1;
+        if (parallel(0, 2, 1, 3)) return 1;
+        if (parallel(0, 3, 1, 2)) return 1;
         return 0;
     }
 
-    private double getLean(int[] coordinate1, int[] coordinate2) {
-        if (coordinate1[0] == coordinate2[0]) return Double.POSITIVE_INFINITY;
-        return (double)(coordinate1[1] - coordinate2[1]) / (coordinate1[0] - coordinate2[0]);
+    private boolean parallel(int a, int b, int c, int d) {
+        int x = (dots[a][0] - dots[b][0]) * (dots[c][1] - dots[d][1]);
+        int y = (dots[a][1] - dots[b][1]) * (dots[c][0] - dots[d][0]);
+        return x == y || x == -y;
     }
 }
