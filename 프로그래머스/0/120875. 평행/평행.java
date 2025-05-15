ccda5c3de442fceb1068e2ecce7d05c5bd6980bc
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-05-15 23:59:46 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,24 +1,24 @@
 class Solution {
     public int solution(int[][] dots) {
-        double slope1, slope2;
+        double line1, line2;
 
-        slope1 = getSlope(dots[0], dots[1]);
-        slope2 = getSlope(dots[2], dots[3]);
-        if (slope1 == slope2) return 1;
+        line1 = getLean(dots[0], dots[1]);
+        line2 = getLean(dots[2], dots[3]);
+        if (line1 == line2) return 1;
 
-        slope1 = getSlope(dots[0], dots[2]);
-        slope2 = getSlope(dots[1], dots[3]);
-        if (slope1 == slope2) return 1;
+        line1 = getLean(dots[0], dots[2]);
+        line2 = getLean(dots[1], dots[3]);
+        if (line1 == line2) return 1;
 
-        slope1 = getSlope(dots[0], dots[3]);
-        slope2 = getSlope(dots[1], dots[2]);
-        if (slope1 == slope2) return 1;
+        line1 = getLean(dots[0], dots[3]);
+        line2 = getLean(dots[1], dots[2]);
+        if (line1 == line2) return 1;
 
         return 0;
     }
 
-    private double getSlope(int[] a, int[] b) {
-        if (a[0] == b[0]) return Double.POSITIVE_INFINITY;
-        return (double)(a[1] - b[1]) / (a[0] - b[0]);
+    private double getLean(int[] coordinate1, int[] coordinate2) {
+        if (coordinate1[0] == coordinate2[0]) return Double.POSITIVE_INFINITY;
+        return (double)(coordinate1[1] - coordinate2[1]) / (coordinate1[0] - coordinate2[0]);
     }
 }
