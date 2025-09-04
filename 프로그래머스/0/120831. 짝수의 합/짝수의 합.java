5762892fa00fae17b4ba018a780daf45c086cf52
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-04 17:08:15 +0900

    [level 0] Title: 짝수의 합, Time: 2.01 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,11 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        int even = 2;
-        while(n >= even) {
-            answer += even;
-            even += 2;
-        }
-        return answer;
+    public int solution(int n) { 
+        return IntStream.rangeClosed(1, n)
+                        .filter(i -> i % 2 == 0)
+                        .sum();
     }
 }
