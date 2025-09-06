f615b383808c7195d3efee15e4a22bda20618f52
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-06 21:05:44 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,5 +1,7 @@
 class Solution {
+    private final int MIN_WIDTH_LENGTH = 2;
+    
     public int solution(String message) {
-        return message.length() * 2;
+        return message.length() * MIN_WIDTH_LENGTH;
     }
 }
