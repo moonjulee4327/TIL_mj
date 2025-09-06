0306a1dc1d874d45659d5193be129486c0cfb0c4
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-06 17:35:40 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,10 +1,8 @@
 class Solution {
     public String solution(String my_string) {
-        char[] my_string_char_arr = my_string.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for(int i = my_string_char_arr.length - 1; i >= 0; i--) {
-            sb.append(my_string_char_arr[i]);
-        }
+        sb.append(my_string);
+        sb.reverse();
         return sb.toString();
     }
 }
