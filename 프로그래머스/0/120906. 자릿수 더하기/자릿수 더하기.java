bc0ff66ce3357b4ee22ba3d1e4c1b097fcccbc6d
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-09 01:21:50 +0900

    [level 0] Title: 자릿수 더하기, Time: 1.84 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,7 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        while(n != 0) {
-            answer += n % 10;
-            n /= 10;
-        }
-        return answer;
+        return Arrays.stream(String.valueOf(n).split("")).mapToInt(Integer::parseInt).sum();
     }
 }
