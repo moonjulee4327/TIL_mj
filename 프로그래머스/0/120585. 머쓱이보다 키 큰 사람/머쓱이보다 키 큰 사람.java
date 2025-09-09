0a346eecc2eaedd997dee760b31e62dc912985ff
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-09 17:47:53 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array, int height) {
-        return (int) Arrays.stream(array).filter(i -> i > height).count();
+        int answer = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > height) answer++; 
+        }
+        return answer;
     }
 }
