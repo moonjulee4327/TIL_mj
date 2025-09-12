b21c1ca0d65e9bce63ade23fac51dfd9a69886c6
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-12 23:40:36 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] dot) {
+        int answer = 0;
+        if(dot[0] > 0) {
+            return dot[1] > 0 ? 1 : 4;
+        }
+        return dot[1] > 0 ? 2 : 3;
+    }
+}
