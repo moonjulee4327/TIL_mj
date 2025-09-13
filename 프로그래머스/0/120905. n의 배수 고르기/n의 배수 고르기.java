211f4fa009ed4f46b08f67563a1ce380c4fec435
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-14 01:21:36 +0900

    [level 0] Title: n의 배수 고르기, Time: 3.79 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,18 +1,7 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int n, int[] numlist) {
-        int length = 0;
-        for(int num : numlist) {
-            if(num % n == 0) {
-                length++;
-            }
-        }
-        int[] answer = new int[length];
-        int idx = 0;
-        for(int i = 0; i < numlist.length; i++) {
-            if(numlist[i] % n == 0) {
-                answer[idx++] = numlist[i];
-            }
-        }
-        return answer;
+        return Arrays.stream(numlist).filter(i -> i % n == 0).toArray();
     }
 }
