44e6c7410674be2956bda3be5eb33635e1ad345d
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-16 16:46:33 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.07 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(int n) {
-        for(int i = 1; i <= 1000; i++) {
-            if(n == i * i) {
-                return 1;
-            }
+        if(n % Math.sqrt(n) == 0) {
+            return 1;
         }
         return 2;
     }
