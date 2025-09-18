92ffc5c43d190f2f05705c92567591edadcf032c
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-18 19:37:00 +0900

    [level 0] Title: 모음 제거, Time: 0.17 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,11 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        for(char c : my_string.toCharArray()) {
-            if(!(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')) {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
