86f26afd272982e3bab839273599bffef5430dfd
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-27 01:53:30 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 5.12 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,12 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        String[] arr = my_string.replaceAll("[a-zA-Z]", "").split("");
-        int[] answer = new int[arr.length];
-        for(int i = 0; i < arr.length; i++) {
-            answer[i] = Integer.parseInt(arr[i]);
-        }
-        Arrays.sort(answer);
-        return answer;
+        return Arrays.stream(my_string.replaceAll("[a-zA-Z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
     }
 }
