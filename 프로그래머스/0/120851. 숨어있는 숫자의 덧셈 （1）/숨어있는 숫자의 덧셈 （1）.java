81e32af21b35ce7f9f3bd7d16b198ad219465ac5
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-09-28 22:45:00 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 1.86 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,10 +1,7 @@
+import java.util.*;
+
 class Solution {
     public int solution(String my_string) {
-        int answer = 0;
-        char[] arr = my_string.replaceAll("[a-z|A-Z]", "").toCharArray();
-        for(char c : arr) {
-            answer += (int) c - '0';
-        }
-        return answer;
+        return Arrays.stream(my_string.replaceAll("[a-z|A-Z]", "").split("")).mapToInt(Integer::parseInt).sum();
     }
 }
