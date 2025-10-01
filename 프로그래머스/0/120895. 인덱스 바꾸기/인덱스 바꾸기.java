49fe2721e49bf8164d0ce90899d909b691e3f18d
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-10-02 02:16:28 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,5 +1,8 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        return my_string.substring(0, num1) + my_string.substring(num2, num2 + 1) + my_string.substring(num1 + 1, num2) + my_string.substring(num1, num1 + 1) + my_string.substring(num2 + 1, my_string.length());
+        char[] arr = my_string.toCharArray();
+        arr[num1] = my_string.charAt(num2);
+        arr[num2] = my_string.charAt(num1);;
+        return String.valueOf(arr);
     }
 }
