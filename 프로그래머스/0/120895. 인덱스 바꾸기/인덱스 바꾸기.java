a0842bb62e9844c975ccc3b79c41bfd5f729a507
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-10-02 01:41:11 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 19.37 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,5 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        return my_string.substring(0, num1) + my_string.substring(num2, num2 + 1) + my_string.substring(num1 + 1, num2) + my_string.substring(num1, num1 + 1) + my_string.substring(num2 + 1, my_string.length());
+    }
+}
