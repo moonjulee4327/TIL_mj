b6629a8fdbc65c93763fc68d878bd66cdb8bc126
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-10-03 13:11:27 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 97.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int maxVal = 0;
+        int maxIdx = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > maxVal) {
+                maxVal = array[i];
+                maxIdx = i;
+            }
+        }
+        return new int[] {
+            maxVal, maxIdx
+        };
+    }
+}
