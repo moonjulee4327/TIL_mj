1a9c27070280e03b42e2ef3cec4d2e4d62cb814d
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-10-05 00:41:08 +0900

    [level 0] Title: 숫자 찾기, Time: 28.52 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,11 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        String[] arr = String.valueOf(num).split("");
-        for(int i = 0; i < arr.length; i++) {
-            if(Integer.parseInt(arr[i]) == k) {
-                return i + 1;
-            }
-        }
-        return -1;
+        return ("$" + num).indexOf(String.valueOf(k));
     }
 }
