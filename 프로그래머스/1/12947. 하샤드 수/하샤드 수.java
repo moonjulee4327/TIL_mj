1565e1a3526ff3520f99cd30602820265a8991f9
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-10-14 00:19:05 +0900

    [level 1] Title: 하샤드 수, Time: 0.18 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean solution(int x) {
+        String[] strArr = String.valueOf(x).split("");
+        int sum = 0;
+        for(String str : strArr) {
+            sum += Integer.parseInt(str);
+        }
+        return x % sum == 0 ? true : false;
+    }
+}
