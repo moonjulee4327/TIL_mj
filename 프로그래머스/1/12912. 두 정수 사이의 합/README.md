8d21b30f79e13fc1449692591fb2c86a3724f3d5
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-10-15 02:36:02 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.08 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/README.md
+++ 프로그래머스/1/12912. 두 정수 사이의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 정수 사이의 합 - 12912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912#) 
 
 ### 성능 요약
 
-메모리: 81.7 MB, 시간: 12.38 ms
+메모리: 75.8 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 15일 02:10:46
+2025년 10월 15일 02:35:59
 
 ### 문제 설명
 
--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -1,14 +1,12 @@
 class Solution {
     public long solution(int a, int b) {
-        long answer = 0;
         if(a > b) {
             int temp = a;
             a = b;
             b = temp;
         }
-        for(int i = a; i <= b; i++) {
-            answer += i;
-        }
-        return answer;
+        // 등차수열의 합
+        // (n(항의 개수) / 2) * (첫번째 항 + 두번째 항)
+        return (long) (b - a + 1) * (a + b) / 2;
     }
 }
