118ce8aed715f503b4c0e1075d273e3f9cd7aadc
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-10-29 02:41:17 +0900

    [level 1] Title: 3진법 뒤집기, Time: 13.48 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,20 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        ArrayList<Integer> arr = new ArrayList<>();
+        String str = "";
         
-        while(n >= 3) {
-            arr.add(n % 3);
+        while(n > 0) {
+            str = str + (n % 3);
             n /= 3;
         }
-        arr.add(n);
         
-        int arrIdx = arr.size() - 1;
-        for(int i = 0; i < arr.size(); i++) {
-            answer += Math.pow(3, arrIdx--) * arr.get(i);
-        }
-        return answer;
+        return Integer.parseInt(str, 3);
     }
 }
