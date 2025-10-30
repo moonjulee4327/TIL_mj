150c0caff5750a9240028554a35ec099bc477252
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-10-31 00:45:22 +0900

    [level 1] Title: 최소직사각형, Time: 1.13 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int temp = 0;
+        for(int i = 0; i < sizes.length; i++) {
+            if(sizes[i][1] > sizes[i][0]) {
+                temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+        }
+        
+        int w = 0;
+        int h = 0;
+        for(int i = 0; i < sizes.length; i++) {
+            if(sizes[i][0] > w) {
+                w = sizes[i][0];
+            }
+            
+            if(sizes[i][1] > h) {
+                h = sizes[i][1];
+            }
+        }
+        
+        return w * h;
+    }
+}
