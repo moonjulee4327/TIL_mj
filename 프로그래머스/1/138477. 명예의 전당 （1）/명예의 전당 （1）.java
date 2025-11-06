50c854b725ef0741978f5c86c76020826bb79ccc
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-11-07 01:48:37 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.85 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,24 +2,18 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        ArrayList<Integer> temp = new ArrayList<Integer>();
-        
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
         for(int i = 0; i < score.length; i++) {
-            arr.add(score[i]);
-            arr.sort(Comparator.reverseOrder());
-            if(k > i) {
-                temp.add(arr.get(i));
-            }else {
-                temp.add(arr.get(k - 1));
+            queue.add(score[i]);
+            
+            if(queue.size() > k) {
+                queue.poll();
             }
+            answer[i] = queue.peek();
         }
-        
-        int[] answer = new int[temp.size()];
-        for(int i = 0; i < temp.size(); i++) {
-            answer[i] = temp.get(i);
-        }
-        
+
         return answer;
     }
 }
