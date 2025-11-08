cbfcba285f0c399cc58276c44de3984d24d676ae
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-11-09 01:17:21 +0900

    [level 1] Title: 추억 점수, Time: 2.24 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,15 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
+        Map<String, Integer> map = new HashMap<>();
         
+        for(int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        int sum;
         for(int i = 0; i < photo.length; i++) {
-            for(int j = 0; j < photo[i].length; j++) {
-                for(int k = 0; k < name.length; k++) {
-                    if(photo[i][j].equals(name[k])) {
-                        answer[i] += yearning[k];
-                    }
+            String[] people = photo[i];
+            sum = 0;
+            for(int j = 0; j < people.length; j++) {
+                if(map.containsKey(people[j])) {
+                    sum += map.get(people[j]); 
                 }
             }
+            answer[i] = sum;
         }
         
         return answer;
