35b6184cf13033269964451a14f43613743cdb11
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-11-14 23:50:29 +0900

    [level 1] Title: 2016년, Time: 1.26 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,29 +1,13 @@
 import java.time.*;
 
 class Solution {
-    public enum WeekDay {
-        SUNDAY("SUN"),
-        MONDAY("MON"),
-        TUESDAY("TUE"),
-        WEDNESDAY("WED"),
-        THURSDAY("THU"),
-        FRIDAY("FRI"),
-        SATURDAY("SAT");
+    private static final String[] SHORT_DAYS = {
+        "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"
+    };
 
-        private final String shortName;
-
-        WeekDay(String shortName) {
-            this.shortName = shortName;
-        }
-
-        public String getShortName() {
-            return shortName;
-        }
-    }
-    
     public String solution(int a, int b) {
         LocalDate date = LocalDate.of(2016, a, b);
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
-        return WeekDay.valueOf(dayOfWeek.name()).getShortName();
+        DayOfWeek day = date.getDayOfWeek();
+        return SHORT_DAYS[day.getValue() - 1];
     }
 }
