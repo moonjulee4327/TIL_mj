26235814bb041cebc7356ad664f510954d6ee59e
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-11-30 03:52:55 +0900

    [level 1] Title: 과일 장수, Time: 5.99 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0;
-        
-        Arrays.sort(score);
-        
-        for(int i = score.length - m; i >= 0; i -= m) {
-            answer += (score[i] * m);
+        int[] sc2 = new int[k+1];
+        for(int i=0; i<score.length; i++) sc2[score[i]]++; 
+        int sum=0, mod=0;
+        for(int i=k; i>0; i--){         
+            sc2[i]=sc2[i] + mod;       
+            sum += sc2[i]/m * m * i;    
+            mod = sc2[i]%m;             
         }
-        
-        return answer;
+        return sum;
     }
 }
