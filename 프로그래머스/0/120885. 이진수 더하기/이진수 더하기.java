519fcd1f46596e396fafa376125dfa9de8422489
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2026-01-09 01:49:13 +0900

    [level 0] Title: 이진수 더하기, Time: 0.05 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,21 +1,8 @@
 class Solution {
     public String solution(String bin1, String bin2) {
-        int i = bin1.length() - 1;
-        int j = bin2.length() - 1;
-        int carry = 0;
-
-        StringBuilder sb = new StringBuilder();
-
-        while (i >= 0 || j >= 0 || carry > 0) {
-            int sum = carry;
-
-            if (i >= 0) sum += bin1.charAt(i--) - '0';
-            if (j >= 0) sum += bin2.charAt(j--) - '0';
-
-            sb.append(sum & 1);
-            carry = sum >> 1;
-        }
-
-        return sb.reverse().toString();
+        int i = Integer.parseInt(bin1, 2);
+        int j = Integer.parseInt(bin2, 2);
+        
+        return Integer.toBinaryString(i + j);
     }
 }
